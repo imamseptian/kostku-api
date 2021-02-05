28ae77c98af0139aec84416238fcdc1804a49f63
imamseptian <imam.septian11187@gmail.com>
2021-02-05 21:45:40 +0700

    storage store

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -8,6 +8,9 @@ use App\Tagihan;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class BarangController extends Controller
 {
