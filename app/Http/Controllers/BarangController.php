d289e934ffc2082b689d63a3a8c57becbf0df371
imamseptian <imam.septian11187@gmail.com>
2021-02-07 23:10:35 +0700

    aaaa

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -20,25 +20,51 @@ class BarangController extends Controller
         $fields = array('number' => $request->number, 'message' => $request->message);
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api-whatsapp-kostku.herokuapp.com/send-message');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, "https://api-whatsapp-kostku.herokuapp.com/send-message");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt(
+            $ch,
+            CURLOPT_POSTFIELDS,
+            "postvar1=value1&postvar2=value2&postvar3=value3"
+        );
+
+        // In real life you should use something like:
+        // curl_setopt($ch, CURLOPT_POSTFIELDS,
+        //          http_build_query(array('postvar1' => 'value1')));
+
+        // Receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($ch);
+
         curl_close($ch);
-        if ($result == "OK") {
-            return response()->json([
-                "code" => 200,
-                "ayaya" => "success"
-            ]);
-        } else {
-            return response()->json([
-                "code" => 500,
-                "ayaya" => "failed"
-            ]);
-        }
+        return response()->json([
+            "code" => 200,
+            "ayaya" => "success"
+        ]);
+
+
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, 'https://api-whatsapp-kostku.herokuapp.com/send-message');
+        // curl_setopt($ch, CURLOPT_POST, true);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        // $result = curl_exec($ch);
+        // curl_close($ch);
+        // if ($result == "OK") {
+        //     return response()->json([
+        //         "code" => 200,
+        //         "ayaya" => "success"
+        //     ]);
+        // } else {
+        //     return response()->json([
+        //         "code" => 500,
+        //         "ayaya" => "failed"
+        //     ]);
+        // }
     }
 
     public function storeGambar(Request $request)
