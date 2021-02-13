23962f70e6caf42e24d4f4396d55599f6b040ed2
imamseptian <imam.septian11187@gmail.com>
2021-02-13 23:18:12 +0700

    aaaa

--- app/Http/Controllers/KostController.php
+++ app/Http/Controllers/KostController.php
@@ -29,7 +29,7 @@ class KostController extends Controller
 
         $data = $data->where('provinsi', 33);
 
-        $data = $data->where('jenis', 3)->get();
+        $data = $data->where('jenis', 1)->get();
 
 
 
