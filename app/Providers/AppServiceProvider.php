3de679881f2514b457dce54b203bfd96a9917e0e
imamseptian <imam.septian11187@gmail.com>
2021-02-13 13:11:45 +0700

    aaaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        Schema::defaultStringLength(191);
+        // Schema
         //
         // $this->app->bind('path.public', function () {
         //     return base_path('../public_html');
